e9bb36d526875a12def669fd64c1a4b5413d2d23
Erik Marty <emarty@broadway.com>
2015-05-06 18:33:43 -0400

    Working on better version

--- szapmart.py
+++ szapmart.py
@@ -10,7 +10,7 @@ def create_app():
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
